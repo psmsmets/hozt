f864ac00f37130c36946ef0ad2d1eaf1622bbdcb
Pieter Smets <mail@pietersmets.be>
2019-02-01 17:52:22 +0100

    added blog post attachments and competition overnightStay to template

---
+++ src/Listener/BlogPostListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Listener;
+
+use App\Entity\BlogPost;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class BlogPostListener
+{
+
+    private $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    
+    public function preUpdate()
+    {
+      
+    }
+}
