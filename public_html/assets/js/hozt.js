4934eac04b93f62f57c4d8da940ca7b5fa56a6b3
Pieter Smets <mail@pietersmets.be>
2019-02-14 09:22:09 +0100

    improved navscroller

--- public_html/assets/js/hozt.js
+++ public_html/assets/js/hozt.js
@@ -15,6 +15,48 @@ function toClipboard(id){
 };
 
 
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#nav-categories').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('#nav-categories').animate({'opacity':'0'},200);
+        $('#nav-categories').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#nav-categories').removeClass('nav-up').addClass('nav-down');
+            $('#nav-categories').animate({'opacity':'1'},200);
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
 $(document).ready(function(){
 
     $(function () {
