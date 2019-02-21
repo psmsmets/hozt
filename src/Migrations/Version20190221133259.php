4daf803de9b24181fd6ce21fd06ef1a2c0671096
Pieter Smets <mail@pietersmets.be>
2019-02-21 14:38:37 +0100

    improved and cleaned competition documents

---
+++ src/Migrations/Version20190221133259.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190221133259 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE competition DROP preprogram, DROP program, DROP results');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE competition ADD preprogram VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD program VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, ADD results VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
