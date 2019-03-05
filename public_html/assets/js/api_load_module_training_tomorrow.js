55678f95f4a5ad7bd012a87d795e95eb46e5deec
Pieter Smets <mail@pietersmets.be>
2019-03-05 11:58:56 +0100

    responsive fix

--- public_html/assets/js/api_load_module_training_tomorrow.js
+++ public_html/assets/js/api_load_module_training_tomorrow.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var module = "api_load_module_training_tomorrow";
     var hoztAPI  = "/api/training/morgen";
 
-   $('#'+module).html("<div class=\"p-3\"><h5 class=\"pb-2 mb-0\">Trainingsuren <span class=\"badge badge-secondary\">morgen</span></h5><div id=\""+module+"_items\"></div></div>");
+   $('#'+module).html("<div class=\"pt-3 pb-3 pl-3 pr-0\"><h5 class=\"pb-2 mb-0\">Trainingsuren <span class=\"badge badge-secondary\">morgen</span></h5><div id=\""+module+"_items\"></div></div>");
 
 
     $.getJSON( hoztAPI, {
@@ -15,8 +15,8 @@ $(document).ready(function(){
     .done(function( training ) {
         if ( training.result ){
             $.each( training.data, function( i, item ) {
-                var t = "<div class=\"row no-gutters\"><div class=\"col-6 text-right pt-1 pr-1 small\">"+ item.time + "</div>";
-                t += "<div class=\"col-6\">";
+                var t = "<div class=\"row no-gutters\"><div class=\"col-5 col-md-9 col-lg-6 text-right pt-1 small\">"+ item.time + "</div>";
+                t += "<div class=\"col-7 col-md-3 col-lg-6 pl-1\">";
                 $.each( item.teams, function( i, team ) {
                     t += "<span class=\"badge badge-hozt badge-team ml-1\">" + team.abbr + "</span>";
                 });
