168c94e89a86b9b1a79e6b409b62e706fc8f59ee
Pieter Smets <mail@pietersmets.be>
2019-09-15 18:48:40 +0200

    roles update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -202,7 +202,7 @@ class User implements UserInterface
     public function isAdmin(): bool
     {
         foreach ($this->roles as $role) {
-            if ($role === 'ROLE_ADMIN' or $role === 'ROLE_SUPERADMIN') return true;
+            if ($role === 'ROLE_ADMIN' or $role === 'ROLE_SUPER_ADMIN') return true;
         }
         return false;
     }
@@ -210,7 +210,7 @@ class User implements UserInterface
     public function isSuperAdmin(): bool
     {
         foreach ($this->roles as $role) {
-            if ($role === 'ROLE_SUPERADMIN') return true;
+            if ($role === 'ROLE_SUPER_ADMIN') return true;
         }
         return false;
     }
