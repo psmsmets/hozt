6ad54839a72c73b90749650fdda12f6e1fc1313b
Pieter Smets <mail@pietersmets.be>
2019-09-26 12:47:11 +0200

    fix

--- assets/js/sportadmin_competition.js
+++ assets/js/sportadmin_competition.js
@@ -8,25 +8,23 @@ function loadCompetitionEnrolments(id) {
     $.getJSON( "/api/private/sportadmin/competition/"+id )
     .done(function( data ) {
         if (data.success) {
-    //var newCompetitions = 0;
-    $.each(data.data, function(i, item) {
-        var td = $('#competition-enrolment-'+item.competitionpart+'-'+item.member);
-        if (item.enabled) {
-            if (item.enrolled) {
-                $(td).html( statusTrue );
-            } else {
-                $(td).html( statusFalse );
-            }
-            td.closest('tr').removeClass('competition-disabled');
-            if (!item.enrolledAt) {
-              td.addClass('table-active');
-            }
-        } else {
-            $(td).html( statusNull );
-        }
-        //if (!item.enrolledAt && item.editable) newCompetitions = newCompetitions + 1;
-    });
-    //if (newCompetitions) $('#competitions-new-tab span').html(newCompetitions);
+            $.each(data.data, function(i, item) {
+                var td = $('#competition-enrolment-'+item.competitionpart+'-'+item.member);
+                if (item.enabled) {
+                    if (item.enrolled) {
+                        $(td).html( statusTrue );
+                    } else {
+                        $(td).html( statusFalse );
+                    }
+                    td.closest('tr').removeClass('competition-disabled');
+                    if (!item.enrolledAt) {
+                        td.addClass('table-active');
+                    }
+                } else {
+                    $(td).html( statusNull );
+                }
+            });
+            $('#competition-enrolments-error').addClass('d-none');
         }
     });
 }
